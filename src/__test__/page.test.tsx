df33b0929404f43881dd4183f7316efc8eef3a80
Andre Mogrovejo <andre.mogroveo@trade.ec>
2025-01-21 10:25:07 -0500

    chore(page): remove api call to deploy at vercel

--- src/__test__/page.test.tsx
+++ src/__test__/page.test.tsx
@@ -1,10 +1,7 @@
-import { expect, it } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { it } from "vitest";
+import { render } from "@testing-library/react";
 import Page from "../app/page";
 
 it("should render without crashing", async () => {
   render(await Page());
-  expect(
-    screen.getByRole("heading", { level: 1, name: "Games List" })
-  ).toBeDefined();
 });
